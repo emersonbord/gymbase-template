94aaf9dfb4d7419407e7bde36a12cad93b15598b
Emerson <emerson.borda.1994@gmail.com>
2022-05-07 17:05:37 -0400

    Avance del proyecto

---
+++ js/app.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    //HERO SLIDER
+    $('#hero-slider').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:true,
+        items: 1,
+        dots: false,
+        smartSpeed: 1250,
+        navText: ['Atrás', 'Siguiente'],
+        responsive:{
+            0:{
+                
+            },
+            600:{
+                
+            },
+            1000:{
+                
+            }
+        }
+    })
+});
