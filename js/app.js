1ace12d4b61081e97e2876332db8cd3ebfc30f13
Emerson <emerson.borda.1994@gmail.com>
2022-09-27 00:05:00 -0300

    70% de avance

--- js/app.js
+++ js/app.js
@@ -10,13 +10,37 @@ $(document).ready(function(){
         navText: ['Atrás', 'Siguiente'],
         responsive:{
             0:{
-                
+                nav: false
             },
-            600:{
-                
+            //con estas dimensiones el botón se muestra de anterior y siguiente
+            768:{
+                nav: true
+            }
+        }
+    })
+
+
+    //PROJECT SLIDER
+    $('#project-slider').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:true,
+        dots: false,
+        smartSpeed: 1000,
+        margin: 24,
+        navText: ["Atrás", "Siguiente"],
+        responsive:{
+            0:{
+                items: 1,
+                nav: false,
+                margin: 0,
             },
-            1000:{
-                
+            768:{
+                items:2,
+            },
+            1140: {
+                items: 2,
+                center: true,
             }
         }
     })
